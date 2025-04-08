a245be097cb1b0cb55eda08d98ce25f2ab5f8e6d
techxudo-office <saadsohail232005@gmail.com>
2025-04-09 00:44:08 +0500

    Fix: sidebar data navigation fixes

--- src/data/sidebarData.jsx
+++ src/data/sidebarData.jsx
@@ -20,6 +20,11 @@ import { IoMdBookmark } from "react-icons/io";
 import { RiRefund2Fill } from "react-icons/ri";
 export const sidebarLinks = [
   {
+    title: "Dashboard",
+    path: "/dashboard",
+    icon: <HiRectangleGroup />,
+  },
+  {
     title: "Companies",
     path: "companies",
     icon: <HiRectangleGroup />,
@@ -72,9 +77,15 @@ export const sidebarLinks = [
     ],
   },
   {
-    title: "Reasons",
-    path: "reasons",
-    icon: <MdOutlineNoteAlt />,
+    title: "Settings",
+    icon: <MdAccountBalanceWallet />,
+    sublinks: [
+      {
+        title: "Reasons",
+        path: "reasons",
+        icon: <MdOutlineNoteAlt />,
+      },
+    ],
   },
   {
     title: "Users",
@@ -85,11 +96,11 @@ export const sidebarLinks = [
         path: "roles",
         icon: <FaSquarePollHorizontal />,
       },
-      {
-        title: "Users",
-        path: "users",
-        icon: <FaUser />,
-      },
+      // {
+      //   title: "Users",
+      //   path: "users",
+      //   icon: <FaUser />,
+      // },
       {
         title: "Admins",
         path: "admins",
@@ -103,123 +114,3 @@ export const sidebarLinks = [
     icon: <MdDoorbell />,
   },
 ];
-
-// import {
-//   MdFlight,
-//   MdOutlineReadMore,
-//   MdSearch,
-//   MdSupervisedUserCircle,
-//   MdContentPasteSearch,
-//   MdAdminPanelSettings,
-//   MdOutlineHistory,
-// } from "react-icons/md";
-// import { HiUsers } from "react-icons/hi";
-// import { useSelector } from "react-redux";
-// import { IoTicket } from "react-icons/io5";
-// import { RiDashboardFill } from "react-icons/ri";
-// import { BiSolidBookmarks } from "react-icons/bi";
-// import { FaBusinessTime, FaMoneyBill1Wave } from "react-icons/fa6";
-
-// export const useSidebarLinks = () => {
-//   const { userData } = useSelector((state) => state.auth);
-//   const pagePermissions = userData?.user?.role?.page_permission || {
-//     dashboard: true,
-//     flights: true,
-//     bookings: true,
-//     credits: true,
-//     transactions: true,
-//     history: true,
-//     administrators: true,
-//     tickets: true,
-//     help_and_support: true,
-//   };
-
-//   return [
-//     pagePermissions.dashboard && {
-//       title: "Dashboard",
-//       path: "/dashboard",
-//       icon: <RiDashboardFill />,
-//     },
-//     pagePermissions.flights && {
-//       title: "Search",
-//       icon: <MdContentPasteSearch />,
-//       sublinks: [
-//         {
-//           title: "Search Flights",
-//           path: "/dashboard/search-flights",
-//           icon: <MdFlight />,
-//         },
-//       ],
-//     },
-//     pagePermissions.bookings && {
-//       title: "Bookings",
-//       icon: <FaBusinessTime />,
-//       sublinks: [
-//         {
-//           title: "Flight Bookings",
-//           path: "/dashboard/flight-bookings",
-//           icon: <BiSolidBookmarks />,
-//         },
-//       ],
-//     },
-//     pagePermissions.transactions && {
-//       title: "Transactions",
-//       icon: <FaMoneyBill1Wave />,
-//       sublinks: [
-//         {
-//           title: "Transaction History",
-//           path: "/dashboard/transactions",
-//           icon: <MdOutlineHistory />,
-//         },
-//       ],
-//     },
-//     pagePermissions.administrators && {
-//       title: "Administrator",
-//       icon: <MdAdminPanelSettings />,
-//       sublinks: [
-//         {
-//           title: "Roles",
-//           path: "/dashboard/roles",
-//           icon: <MdSupervisedUserCircle />,
-//         },
-//         {
-//           title: "Users",
-//           path: "/dashboard/users",
-//           icon: <HiUsers />,
-//         },
-//       ],
-//     },
-//     pagePermissions.tickets && {
-//       title: "Support",
-//       icon: <IoTicket />,
-//       sublinks: [
-//         {
-//           title: "Tickets",
-//           path: "/dashboard/view-tickets",
-//           icon: <IoTicket />,
-//         },
-//       ],
-//     },
-//     pagePermissions.history && {
-//       title: "History",
-//       icon: <IoTicket />,
-//       sublinks: [
-//         {
-//           title: "Search",
-//           path: "/dashboard/search-history",
-//           icon: <MdSearch />,
-//         },
-//         {
-//           title: "Booking",
-//           path: "/dashboard/booking-history",
-//           icon: <IoTicket />,
-//         },
-//         {
-//           title: "Order",
-//           path: "/dashboard/order-history",
-//           icon: <MdOutlineReadMore />,
-//         },
-//       ],
-//     },
-//   ].filter(Boolean);
-// };
