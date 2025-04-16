e5aee465ab8b91968ac43837568222fd7fce5957
M-Saad-Sohail <saadsohail232005@gmail.com>
2025-04-17 04:49:28 +0500

    Feat: changed base url link to live

--- src/utils/ApiBaseUrl.js
+++ src/utils/ApiBaseUrl.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:3004"
-// const BASE_URL = "https://admin.skyight.com"
+// const BASE_URL = "http://localhost:3004"
+const BASE_URL = "https://admin.skyight.com"
 
 export { BASE_URL };
