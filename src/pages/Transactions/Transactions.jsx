2e87fd7daa52dd7e5294dc6975f1fddea385f8e7
M.Saad Sohail <saadsohail232005@gmail.com>
2025-05-16 16:39:24 +0500

    Feat: invalid api response issue resolved

--- src/pages/Transactions/Transactions.jsx
+++ src/pages/Transactions/Transactions.jsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import {
   Button,
   CustomTooltip,
   ModalWrapper,
-  MultiSelect,
   Searchbar,
-  SecondaryButton,
   Table,
   Tag,
 } from "../../components/components";
