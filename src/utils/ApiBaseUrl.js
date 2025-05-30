d76e5db659db2b1214d8daa94804f7d44f2ed576
M.Saad Sohail <saadsohail232005@gmail.com>
2025-05-30 14:20:24 +0500

    Feat: live url update

--- src/utils/ApiBaseUrl.js
+++ src/utils/ApiBaseUrl.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:3004"
+// const BASE_URL = "http://localhost:3004"
 // const BASE_URL = "https://g4n8vxxb-3004.inc1.devtunnels.ms";
-// const BASE_URL = "https://admin.skyight.com"
+const BASE_URL = "https://admin.skyight.com"
 
 export { BASE_URL };
