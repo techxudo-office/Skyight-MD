a0f1046d5c6f4fb53ba7f634b869763f885d90ee
M.Saad Sohail <saadsohail232005@gmail.com>
2025-07-05 16:36:20 +0500

    Fix: redirect fixes of login to dashboard and vice versa

--- src/components/LoginForm/LoginForm.jsx
+++ src/components/LoginForm/LoginForm.jsx
@@ -68,82 +68,80 @@ const LoginForm = () => {
   return (
     <>
       {/* Render form only if user is not logged in */}
-      {/* {!adminData && ( */}
-      <CardLayoutContainer className={"max-w-md p-3 m-auto shadow-2xl"}>
-        <CardLayoutHeader
-          heading="Login"
-          className={"flex items-center justify-center pt-7 pb-0"}
-          removeBorder={true}
-        />
-        <form onSubmit={handleFormSubmit} noValidate>
-          <CardLayoutBody removeBorder={true}>
-            <div>
-              <div className="flex flex-col gap-5">
-                {/* Email input with validation error */}
-                <div
-                  className={`relative ${
-                    formik.touched.email && formik.errors.email ? "mb-5" : ""
-                  }`}
-                >
-                  <Input
-                    placeholder={"abc.xcv@gmail.com"}
-                    id={"email"}
-                    name={"email"}
-                    label={"Email Address*"}
-                    type={"email"}
-                    value={formik.values.email}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                  />
-                  {formik.touched.email && formik.errors.email && (
-                    <div className="absolute left-0 mt-2 text-sm text-red-500">
-                      {formik.errors.email}
-                    </div>
-                  )}
-                </div>
+        <CardLayoutContainer className={"max-w-md p-3 m-auto shadow-2xl"}>
+          <CardLayoutHeader
+            heading="Login"
+            className={"flex items-center justify-center pt-7 pb-0"}
+            removeBorder={true}
+          />
+          <form onSubmit={handleFormSubmit} noValidate>
+            <CardLayoutBody removeBorder={true}>
+              <div>
+                <div className="flex flex-col gap-5">
+                  {/* Email input with validation error */}
+                  <div
+                    className={`relative ${
+                      formik.touched.email && formik.errors.email ? "mb-5" : ""
+                    }`}
+                  >
+                    <Input
+                      placeholder={"abc.xcv@gmail.com"}
+                      id={"email"}
+                      name={"email"}
+                      label={"Email Address*"}
+                      type={"email"}
+                      value={formik.values.email}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.email && formik.errors.email && (
+                      <div className="absolute left-0 mt-2 text-sm text-red-500">
+                        {formik.errors.email}
+                      </div>
+                    )}
+                  </div>
 
-                {/* Password input with validation error */}
-                <div
-                  className={`relative ${
-                    formik.touched.password && formik.errors.password
-                      ? "mb-5"
-                      : ""
-                  }`}
-                >
-                  <Input
-                    placeholder={"********"}
-                    id={"password"}
-                    name={"password"}
-                    label={"Password*"}
-                    type={"password"}
-                    value={formik.values.password}
-                    onChange={formik.handleChange}
-                    onBlur={formik.handleBlur}
-                  />
-                  {formik.touched.password && formik.errors.password && (
-                    <div className="absolute left-0 mt-2 text-sm text-red-500">
-                      {formik.errors.password}
-                    </div>
-                  )}
+                  {/* Password input with validation error */}
+                  <div
+                    className={`relative ${
+                      formik.touched.password && formik.errors.password
+                        ? "mb-5"
+                        : ""
+                    }`}
+                  >
+                    <Input
+                      placeholder={"********"}
+                      id={"password"}
+                      name={"password"}
+                      label={"Password*"}
+                      type={"password"}
+                      value={formik.values.password}
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                    />
+                    {formik.touched.password && formik.errors.password && (
+                      <div className="absolute left-0 mt-2 text-sm text-red-500">
+                        {formik.errors.password}
+                      </div>
+                    )}
+                  </div>
                 </div>
               </div>
-            </div>
-          </CardLayoutBody>
+            </CardLayoutBody>
 
-          <CardLayoutFooter className={"flex items-center justify-center"}>
-            <div>
-              {/* Show spinner while loading, otherwise show "Login" button */}
-              <Button
-                text={isLoading ? <Spinner /> : "Login"}
-                disabled={isLoading}
-                onClick={formik.handleSubmit}
-                type="submit"
-              />
-            </div>
-          </CardLayoutFooter>
-        </form>
-      </CardLayoutContainer>
-      {/* )} */}
+            <CardLayoutFooter className={"flex items-center justify-center"}>
+              <div>
+                {/* Show spinner while loading, otherwise show "Login" button */}
+                <Button
+                  text={isLoading ? <Spinner /> : "Login"}
+                  disabled={isLoading}
+                  onClick={formik.handleSubmit}
+                  type="submit"
+                />
+              </div>
+            </CardLayoutFooter>
+          </form>
+        </CardLayoutContainer>
     </>
   );
 };
--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,12 +1,25 @@
 import "./Login.css";
-import  LoginForm  from "../../components/LoginForm/LoginForm";
+import LoginForm from "../../components/LoginForm/LoginForm";
+import { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const Login = () => {
+  const navigate = useNavigate();
+  const { adminData } = useSelector((state) => state.persist);
+
+  useEffect(() => {
+    if (adminData?.token) {
+      navigate("/dashboard"); // Redirect to dashboard if user is already logged in
+    }
+  }, [adminData, navigate]);
   return (
     <>
-      <div className="flex items-center justify-center w-full h-screen login-page bg-slate-100">
-        <LoginForm />
-      </div>
+      {!adminData?.token && (
+        <div className="flex items-center justify-center w-full h-screen login-page bg-slate-100">
+          <LoginForm />
+        </div>
+      )}
     </>
   );
 };
