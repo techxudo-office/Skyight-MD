734f6a1a2356cd3ee6eab350f33aa493581a2059
hashamuddin1 <hashamlaptop@gmail.com>
2025-08-28 18:19:06 +0500

    change socket url

--- src/utils/ApiBaseUrl.js
+++ src/utils/ApiBaseUrl.js
@@ -1,5 +1,5 @@
 // const BASE_URL = "http://localhost:3004";
-const SOCKET_URL = "http://localhost:3000";
+const SOCKET_URL = "https://api.skyight.com";
 const BASE_URL = "https://admin.skyight.com"
 
 export { BASE_URL, SOCKET_URL };
